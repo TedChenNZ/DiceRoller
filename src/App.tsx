60224c3c5dab673767a7cbac0b14be379130d7f1
Ted Chen <tedchennz@gmail.com>
2019-05-18 23:48:25 +1200

    Cleanup styles, add defaults and result in query

--- src/App.tsx
+++ src/App.tsx
@@ -3,81 +3,71 @@ import "reset-css";
 import { IMobDamageResult, rollMobDamageResults } from "./dndRolls";
 import * as styles from "./styles.scss";
 import MobResult from "./components/MobResults/MobResult";
+import MobRollerForm from "./components/MobRollerForm/MobRollerForm";
+import queryString from "query-string";
+
+const onComponentMount = (onMount: () => void) => {
+  const [mounted, setMount] = useState(false);
+  if (!mounted) {
+    onMount();
+    setMount(true);
+  }
+};
 
 const App = () => {
+  const [defaultValues, setDefaultValues] = useState({});
   const [history, setHistory] = useState([] as IMobDamageResult[]);
+  onComponentMount(() => {
+    const {
+      mobDamageResult: mobDamageResultBase64,
+      ...defaults
+    } = queryString.parse(location.search);
+    setDefaultValues(defaults);
+    let mobDamageResult;
+    try {
+      if (mobDamageResultBase64) {
+        mobDamageResult = JSON.parse(atob(mobDamageResultBase64));
+      }
+    } catch {}
+    const newHistory = mobDamageResult
+      ? [mobDamageResult, ...history]
+      : history;
+    setHistory(newHistory);
+  });
+
   const onSubmit = (e: any) => {
-    const form: HTMLFormElement = e.target.form;
-    if (!form.checkValidity()) {
+    const formElement: HTMLFormElement = e.target.form;
+    if (!formElement.checkValidity()) {
       return;
     }
     const inputs = {
-      mobSize: parseInt(form.mobSize.value, 10),
-      toHitMod: parseInt(form.toHitMod.value, 10),
-      damageDice: form.damageDice.value,
-      ac: parseInt(form.ac.value, 10),
-      advantage: form.advantage.checked,
-      disadvantage: form.disadvantage.checked
+      mobSize: parseInt(formElement.mobSize.value, 10),
+      toHitMod: parseInt(formElement.toHitMod.value, 10),
+      damageDice: formElement.damageDice.value,
+      ac: parseInt(formElement.ac.value, 10),
+      advantage: formElement.advantage.checked,
+      disadvantage: formElement.disadvantage.checked
     };
     e.preventDefault();
     const mobDamageResult = rollMobDamageResults(inputs);
     setHistory([mobDamageResult, ...history]);
+    const url = queryString.parseUrl(location.href).url;
+    window.history.pushState(
+      inputs,
+      document.title,
+      `${url}?${queryString.stringify({
+        ...inputs,
+        mobDamageResult: btoa(JSON.stringify(mobDamageResult))
+      })}`
+    );
   };
+
   const mobResult = history && history.length ? history[0] : undefined;
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>D&amp;D Mob Roller</h1>
       <div className={styles.input}>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="mobSize">Mob Size</label>
-          <input
-            id="mobSize"
-            name="mobSize"
-            className={styles.box}
-            autoFocus
-            type="number"
-            defaultValue="10"
-            required
-          />
-
-          <label htmlFor="toHitMod">To Hit Mod</label>
-          <input
-            id="toHitMod"
-            name="toHitMod"
-            className={styles.box}
-            type="number"
-            defaultValue="4"
-            required
-          />
-
-          <label htmlFor="damageDice">Damage Dice</label>
-          <input
-            id="damageDice"
-            name="damageDice"
-            className={styles.box}
-            type="text"
-            defaultValue="1d6 + 1"
-            required
-          />
-
-          <label htmlFor="ac">Target AC</label>
-          <input
-            id="ac"
-            name="ac"
-            className={styles.box}
-            type="number"
-            defaultValue="12"
-            required
-          />
-
-          <label htmlFor="advantage">Advantage</label>
-          <input id="advantage" name="advantage" type="checkbox" />
-
-          <label htmlFor="disadvantage">Disadvantage</label>
-          <input id="disadvantage" name="disadvantage" type="checkbox" />
-
-          <button onClick={onSubmit}>Roll Dice</button>
-        </form>
+        <MobRollerForm defaultValues={defaultValues} onSubmit={onSubmit} />
         <MobResult mobResult={mobResult} />
       </div>
     </div>
---
+++ src/components/MobRollerForm/MobRollerForm.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { IMobDamageInput } from "../../dndRolls";
+
+interface ITextInput extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+const TextInput = ({ label, name, ...inputProps }: ITextInput) => {
+  return (
+    <React.Fragment>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} name={name} {...inputProps} />
+    </React.Fragment>
+  );
+};
+
+const defaultDefaultValues = {
+  mobSize: 10,
+  toHitMod: 4,
+  damageDice: "1d6 + 2",
+  ac: 15,
+  advantage: false,
+  disadvantage: false,
+  average: false
+};
+const MobRollerForm = ({
+  defaultValues,
+  onSubmit
+}: {
+  defaultValues;
+  onSubmit: (e) => any;
+}) => {
+  const defaults = {
+    mobSize: defaultValues.mobSize || defaultDefaultValues.mobSize,
+    toHitMod: defaultValues.toHitMod || defaultDefaultValues.toHitMod,
+    damageDice: defaultValues.damageDice || defaultDefaultValues.damageDice,
+    ac: defaultValues.ac || defaultDefaultValues.ac,
+    advantage: defaultValues.advantage,
+    disadvantage: defaultValues.disadvantage
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <TextInput
+        label="Mob Size"
+        name="mobSize"
+        type="number"
+        defaultValue={defaults.mobSize}
+        required
+        autoFocus
+      />
+      <TextInput
+        label="To Hit Mod"
+        name="toHitMod"
+        type="number"
+        defaultValue={defaults.toHitMod}
+        required
+      />
+      <TextInput
+        label="Damage Dice"
+        name="damageDice"
+        type="text"
+        defaultValue={defaults.damageDice}
+        required
+      />
+      <TextInput
+        label="Target AC"
+        name="ac"
+        type="number"
+        defaultValue={defaults.ac}
+        required
+      />
+
+      <label htmlFor="advantage">Advantage</label>
+      <input
+        id="advantage"
+        name="advantage"
+        type="checkbox"
+        defaultChecked={defaults.advantage}
+      />
+
+      <label htmlFor="disadvantage">Disadvantage</label>
+      <input
+        id="disadvantage"
+        name="disadvantage"
+        type="checkbox"
+        defaultChecked={defaults.disadvantage}
+      />
+
+      <button onClick={onSubmit}>Roll Dice</button>
+    </form>
+  );
+};
+
+export default MobRollerForm;
